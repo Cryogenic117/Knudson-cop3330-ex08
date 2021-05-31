d1b60daebf71acf4e5ec6fbcd90e3c21291c4a4e
Brandon Knudson <bknudson117@gmail.com>
2021-05-31 15:59:13 -0400

    1

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,22 @@
+package org.example;
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner input = new Scanner(System.in);
+        int people, pizzas, slices;
+        System.out.println("How many people? ");
+        people = input.nextInt();
+        System.out.println("How many pizzas do you have? ");
+        pizzas = input.nextInt();
+        System.out.println("How many slices per pizza? ");
+        slices = input.nextInt();
+
+
+        System.out.printf("%d people with %d pizzas (%d slices)%n", people, pizzas, slices * pizzas);
+        System.out.printf("Each person gets %d pieces of pizza.%n", (slices * pizzas)/ people);
+        System.out.printf("There are %d leftover pieces", (slices * pizzas) % people);
+    }
+}
